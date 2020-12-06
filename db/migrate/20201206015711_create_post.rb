4a97d61e9fdec6b74a2a88ccf7c26cc6ee10f9ba
fillipesilva92 <f.fillipesilva@gmail.com>
2020-12-06 00:14:23 -0300

    Crud Posts

---
+++ db/migrate/20201206015711_create_post.rb
@@ -0,0 +1,9 @@
+class CreatePost < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.string :image
+    end
+  end
+end
