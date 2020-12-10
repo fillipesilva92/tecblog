31b61ed000a12020a1f8fd6e86a64a717e937476
fillipesilva92 <f.fillipesilva@gmail.com>
2020-12-10 14:52:24 -0300

    List active categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,9 @@
 class CategoriesController < ApplicationController
   def filtered
     @category = Category.find_by_name!(params[:name])
+    
+    if @category.status == 'inativo'
+      redirect_to '/', notice: 'Inactive Category'
+    end
   end
 end
